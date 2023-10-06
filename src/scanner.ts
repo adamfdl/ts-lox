9a8ec03df726ea00137c28fe6752909cec8431c3
xen.n-adam <xen.n-adam@MBP-N26DG3DYHX.local>
2023-10-06 12:07:05 +0700

    scanner

--- src/scanner.ts
+++ src/scanner.ts
@@ -28,7 +28,6 @@ export class Scanner {
         this.keywords.set('or', TokenType.OR);
         this.keywords.set('print', TokenType.PRINT);
         this.keywords.set('return', TokenType.RETURN);
-
         this.keywords.set('super', TokenType.SUPER);
         this.keywords.set('this', TokenType.THIS);
         this.keywords.set('true', TokenType.TRUE);
@@ -79,8 +78,53 @@ export class Scanner {
             case '*':
                 this.addToken(TokenType.STAR, null);
                 break;
+            case '!':
+                this.addToken(this.match('=') ? TokenType.BANG_EQUAL : TokenType.BANG, null);
+                break;
+            case '=':
+                this.addToken(this.match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL, null);
+                break;
+            case '<':
+                this.addToken(this.match('=') ? TokenType.LESS_EQUAL : TokenType.LESS, null);
+                break;
+            case '>':
+                this.addToken(this.match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER, null);
+                break;
+            // Longer lexemes
+            case '/':
+                // This is similar to the other two-character operators, except that when we 
+                // find a second /, we don't end the token yet. Instead, we keep consuming
+                // characters until we reach the end of the line.
+                //
+                // Comments are lexemes that the scanner ignores. They are not tokens, so we 
+                // don't add them to the list of tokens. We just consume characters until we
+                // reach the end of the line.
+                if (this.match('/')) {
+                    while (this.peek() != '\n' && !this.isAtEnd()) this.advance();
+                } else {
+                    this.addToken(TokenType.SLASH, null);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace.
+                break;
+            case '\n':
+                this.line++;
+                break;
+            case '"':
+                this.string();
+                break;
             default:
-                console.error(`Unexpected character ${c} at line ${this.line}`);
+                if (this.isDigit(c)) {
+                    this.number();
+                } else if (this.isAlpha(c)) {
+                    this.identifier();
+                } else {
+                    console.error(`Unexpected character [${c}] at line ${this.line}`);
+                }
+                break;
         }
     }
 
@@ -95,5 +139,89 @@ export class Scanner {
     private addToken(type: TokenType, literal: Object | null): void {
         const text: string = this.source.substring(this.start, this.current);
         this.tokens.push(new Token(type, text, literal, this.line));
+    }
+
+    private match(expected: string): boolean {
+        if (this.isAtEnd()) return false;
+        if (this.source.charAt(this.current) !== expected) return false;
+
+        this.current++;
+        return true;
+    }
+
+    // peek is sort of like advance but doesn't consume the character. This is called
+    // lookahead.
+    private peek(): string {
+        if (this.isAtEnd()) return '\0';
+        return this.source.charAt(this.current);
+    }
+
+    private peekNext(): string {
+        if (this.current + 1 >= this.source.length) return '\0';
+        return this.source.charAt(this.current + 1);
+    }
+
+    private string(): void {
+        while (this.peek() != '"' && !this.isAtEnd()) {
+            if (this.peek() == '\n') this.line++;
+            this.advance();
+        }
+
+        if (this.isAtEnd()) {
+            // TODO: Format this error with line number also
+            console.error("Unterminated string.")
+            return
+        }
+
+        // The closing ".
+        this.advance();
+
+        // Trim the surrounding quotes.
+        const value: string = this.source.substring(this.start + 1, this.current - 1);
+        this.addToken(TokenType.STRING, value);
+    }
+
+    private isDigit(character: string): boolean {
+        return character >= '0' && character <= '9';
+    }
+
+    private number(): void {
+        while (this.isDigit(this.peek())) this.advance();
+
+        if (this.peek() === '.' && this.isDigit(this.peekNext())) {
+            this.advance();
+
+            while (this.isDigit(this.peek())) this.advance();
+        }
+
+        this.addToken(
+            TokenType.NUMBER,
+            parseFloat(this.source.substring(this.start, this.current))
+        )
+    }
+
+    private identifier(): void {
+        while (this.isAlphaNumeric(this.peek())) this.advance();
+
+        // See if the identifier is a reserved word.
+        const text: string = this.source.substring(this.start, this.current);
+
+        const keyword: TokenType | undefined = this.keywords.get(text);
+        if (keyword !== undefined) {
+            this.addToken(keyword, null);
+            return;
+        }
+
+        this.addToken(TokenType.IDENTIFIER, null);
+    }
+
+    private isAlpha(character: string): boolean {
+        return (character >= 'a' && character <= 'z') ||
+            (character >= 'A' && character <= 'Z') ||
+            character === '_';
+    }
+
+    private isAlphaNumeric(character: string): boolean {
+        return this.isAlpha(character) || this.isDigit(character);
     }
 }
--- test/scanner.spec.ts
+++ test/scanner.spec.ts
@@ -1,4 +1,4 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 
 import { Scanner } from '../src/scanner';
 
@@ -20,4 +20,62 @@ describe('Scanner', () => {
         expect(tokens[9].type).to.equal('STAR');
         expect(tokens[10].type).to.equal('EOF');
     });
+
+    describe('operator scanners', () => {
+        describe('should scan bang operators', () => {
+            it('should scan bang equal', () => {
+                const tokens = new Scanner('!=').scanTokens();
+                expect(tokens[0].type).to.equal('BANG_EQUAL');
+            });
+            it('should scan bang', () => {
+                const tokens = new Scanner('!').scanTokens();
+                expect(tokens[0].type).to.equal('BANG');
+            });
+        });
+        describe('should scan equal operators', () => {
+            it('should scan equal', () => {
+                const tokens = new Scanner('=').scanTokens();
+                expect(tokens[0].type).to.equal('EQUAL');
+            });
+            it('should scan equal equal', () => {
+                const tokens = new Scanner('==').scanTokens();
+                expect(tokens[0].type).to.equal('EQUAL_EQUAL');
+            });
+        });
+
+    });
+
+    describe('comments scanner', () => {
+        it('should scan comments', () => {
+            const tokens = new Scanner('// this is a comment').scanTokens();
+            expect(tokens[0].type).to.equal('EOF');
+        });
+    })
+
+    describe('number scanner', () => {
+        it('should scan numbers', () => {
+            const tokens = new Scanner('123').scanTokens();
+            expect(tokens[0].type).to.equal('NUMBER');
+        });
+        it('should scan decimal numbers', () => {
+            const tokens = new Scanner('123.1').scanTokens();
+            expect(tokens[0].type).to.equal('NUMBER');
+        });
+    });
+
+    describe('string scanner', () => {
+        it('should scan strings', () => {
+            const tokens = new Scanner('"hello world"').scanTokens();
+            expect(tokens[0].type).to.equal('STRING');
+
+        });
+        it('should scan identifiers', () => {
+            const tokens = new Scanner('my_var').scanTokens();
+            expect(tokens[0].type).to.equal('IDENTIFIER');
+        });
+        it('should scan reserved words', () => {
+            const tokens = new Scanner('and').scanTokens();
+            expect(tokens[0].type).to.equal('AND');
+        });
+    })
 });
