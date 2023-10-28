5ec225237e2eaeddfe77eddaad0498ed5823d11a
xen.n-adam <xen.n-adam@MBP-N26DG3DYHX.local>
2023-10-28 18:57:58 +0700

    scoping

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,14 @@ import { Token } from "./token";
 
 
 export class Environment {
+
+    private readonly enclosing: Environment | null = null;
+
     private readonly values: Map<string, any> = new Map<string, any>();
+
+    constructor(enclosing?: Environment) {
+        this.enclosing = enclosing || null;
+    }
 
     public define(name: string, value: any): void {
         this.values.set(name, value);
@@ -14,12 +21,23 @@ export class Environment {
             return this.values.get(name.lexeme);
         }
 
+        if (this.enclosing !== null) {
+            // This will recursively call get() from this class until it finds the variable
+            // if not found, it will throw an error
+            return this.enclosing.get(name);
+        }
+
         throw new RuntimeError(name, `Undefined variable ${name.lexeme}.`)
     }
 
     public assign(name: Token, value: any) {
         if (this.values.has(name.lexeme)) {
             this.values.set(name.lexeme, value);
+            return;
+        }
+
+        if (this.enclosing !== null) {
+            this.enclosing.assign(name, value);
             return;
         }
 
